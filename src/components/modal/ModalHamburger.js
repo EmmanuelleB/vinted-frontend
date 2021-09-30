f0da775cd0c196e7fbd29a180e624f23bb6e96d8
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-09-30 11:19:16 +0200

    import all packages

--- src/components/modal/ModalHamburger.js
+++ src/components/modal/ModalHamburger.js
@@ -1,44 +1,42 @@
 import "./ModalHamburger.scss";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 const ModalHamburger = (props) => {
-    const {token ,handleSend, handleOpenSignupModal, handleOpenLoginModal, handleLogOut, handleCloseHamburgerModal} = props;
+  const { token, handleSend, handleOpenSignupModal, handleOpenLoginModal, handleLogOut, handleCloseHamburgerModal } =
+    props;
   return (
- <div className="modal">
-    <div className="page-container">
+    <div className="modal">
+      <div className="page-container">
         <div className="menuForm">
-       
-      
-             {token ? null : (
-                  <button onClick={handleOpenSignupModal} className="button-border-blue">
-                    S'inscrire
-                  </button>
-                )}
+          {token ? null : (
+            <button onClick={handleOpenSignupModal} className="button-border-blue">
+              S'inscrire
+            </button>
+          )}
 
-               {token ? (
-                  <button onClick={handleLogOut} className="button-pink">
-                    Se Déconnecter
-                  </button>
-                ) : (
-                  <button onClick={handleOpenLoginModal} className="button-border-blue">
-                    Se connecter
-                  </button>
-                )}
-               {token ? (
-                <Link to="/offer/publish">
-                  <button className="button-blue" onClick={handleCloseHamburgerModal}>Vends tes articles</button>
-                </Link>
-              ) : (
-                <button className="button-blue" onClick={handleSend}>
-                  Vends tes articles
-                </button>
-              )}
+          {token ? (
+            <button onClick={handleLogOut} className="button-pink">
+              Se Déconnecter
+            </button>
+          ) : (
+            <button onClick={handleOpenLoginModal} className="button-border-blue">
+              Se connecter
+            </button>
+          )}
+          {token ? (
+            <Link to="/offer/publish">
+              <button className="button-blue" onClick={handleCloseHamburgerModal}>
+                Vends tes articles
+              </button>
+            </Link>
+          ) : (
+            <button className="button-blue" onClick={handleSend}>
+              Vends tes articles
+            </button>
+          )}
         </div>
-    
+      </div>
     </div>
-  </div>
-  )
-
-  
-}
+  );
+};
 
 export default ModalHamburger;
